aa71ef58198959c7bfeda37d8b7ebf6f3ca168f9
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2019-05-10 20:13:13 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,5 @@
 $(document).ready(function () {
 
-     	// Change the title text when leaving the tab
-		$(function() {
-			     // Get page title
-				var pageTitle = $("title").text();
-
-			// Change page title on blur
-			$(window).blur(function() {
-				$("title").text("{ Your next Front-End developer }");
-			});
-
-			// Change page title back on focus
-			$(window).focus(function() {
-				$("title").text(pageTitle);
-			});
-		});
-
-
 		// Fade in elements
 
 		$(function() {
